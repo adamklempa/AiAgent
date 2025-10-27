52055f8435d37c7baa7e34e965bbd89254f8bb0e
adamklempa <adamklempa@gmail.com>
2025-10-27 08:37:19 -0500

    added command line functionality

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import os
+import sys
 from google import genai
 from dotenv import load_dotenv
+from google.genai import types
 
 
 def main():
+    if len(sys.argv) < 2:
+        print("No valid input, please type uv run main.py followed by your command")
+        sys.exit(1)
+    user_prompt = sys.argv[1]
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=messages
     )
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
     print(response.text)
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
 
 
 if __name__ == "__main__":
-    main()cd
+    main()
